7d89ebdf047bd7ca6f492d4544eb0d31646e224b
James Banting <jbanting@sparkgeo.com>
2018-08-14 17:08:28 -0700

    added validator

--- stac_validator.py
+++ stac_validator.py
@@ -23,88 +23,22 @@ def stac_validate(root_catalog):
     :param args: 
     :return: 
     """
+    print(root_catalog)
     instance = requests.get(root_catalog).json()
     ITEM_SCHEMA = requests.get(ITEM_SCHEMA_URL).json()
+    CATALOG_SCHEMA = requests.get(CATALOG_SCHEMA_URL).json()
     stac_validator = validate(instance, ITEM_SCHEMA)
-    
+
 
 @app.route("/api/validate", methods=["GET", "POST"])
 def api_validate():
     if flask_request.method == "POST":
-        if flask_request.form != {}:
-            if "url" in flask_request.form and flask_request.form["url"] != "":
-                args = {}
-                for k in flask_request.form:
-                    if k != "local_filename":
-                        args[k] = flask_request.form[k]
-                return validate(args)
-
-            if "filename" in flask_request.form:
-                url = flask_request.form["filename"]
-            else:
-                url = "unknown_file_name"
-
-            if "file_b64" not in flask_request.form:
-                return (
-                    json.dumps(
-                        {
-                            "status": "failure",
-                            "error": 'Missing "file_b64" field in POSTed form data',
-                        }
-                    ),
-                    400,
-                    {"Content-Type": "application/json"},
-                )
-
-           
-            except Exception as e:
-                return (
-                    json.dumps(
-                        {
-                            "status": "failure",
-                            "error": "Invalid content for file_b64: %s" % str(e),
-                        }
-                    ),
-                    400,
-                    {"Content-Type": "application/json"},
-                )
-
-            open(tmpfilename, "wb").write(decoded)
-        else:
-            if "file" not in flask_request.files:
-                return (
-                    json.dumps(
-                        {
-                            "status": "failure",
-                            "error": 'Missing "file" field in POSTed form data',
-                        }
-                    ),
-                    400,
-                    {"Content-Type": "application/json"},
-                )
-            f = flask_request.files["file"]
-            if f.filename == "":
-                return (
-                    json.dumps(
-                        {
-                            "status": "failure",
-                            "error": 'Missing "file" field in POSTed form data',
-                        }
-                    ),
-                    400,
-                    {"Content-Type": "application/json"},
-                )
-            f.save(tmpfilename)
-            url = f.filename
-
-        try:
-            return validate({"local_filename": tmpfilename, "url": url})
-        finally:
-            os.unlink(tmpfilename)
-
-    else:
         args = {}
         for k in flask_request.args:
-            if k != "local_filename":
+            print(k)
+            if k == "stac_catalog":
                 args[k] = flask_request.args[k]
-        return validate(args)
+                print(args.get('stac_catalog'))
+        return stac_validate(args.get('stac_catalog'))
+    else:
+        return 'HELLO'
