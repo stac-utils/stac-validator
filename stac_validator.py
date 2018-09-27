fa112c069ac8c52001cd49e5aa6b4aff7d423268
Darren Wiens <dkwiens@gmail.com>
2018-09-27 16:46:11 -0700

    wip multiprocessing

--- stac_validator.py
+++ stac_validator.py
@@ -23,10 +23,10 @@ import traceback
 import json
 import requests
 from docopt import docopt
-
+import multiprocessing
 
 class StacValidate:
-    def __init__(self, stac_file, version="master", verbose=False):
+    def __init__(self, stac_file, verbose_dict, summary_dict, summary_lock, version="master", verbose=False):
         """
         Validate a STAC file
         :param stac_file: file to validate
@@ -45,6 +45,7 @@ class StacValidate:
         )
 
         self.stac_file = stac_file.strip()
+        self.path = stac_file.strip()
         self.ITEM_SCHEMA = requests.get(ITEM_SCHEMA_URL).json()
         self.CATALOG_SCHEMA = requests.get(CATALOG_SCHEMA_URL).json()
         self.fpath = Path(stac_file)
@@ -60,16 +61,15 @@ class StacValidate:
             }
         }
 
-        self.run()
+        self.run(verbose_dict, summary_dict, summary_lock)
 
-    def validate_stac(self, stac_file, schema):
+    def validate_stac(self, stac_file, schema, verbose_dict, summary_dict, summary_lock):
         """
         Validate stac
         :param stac_file: input stac_file
         :param stac_type of STAC (item, catalog)
         :return: validation message
         """
-
         try:
             validate(stac_file, schema)
             self.message["valid_stac"] = True
@@ -83,34 +83,36 @@ class StacValidate:
         except Exception as error:
             self.message["valid_stac"] = False
             self.message["error"] = f"{error}"
+        verbose_dict[self.path] = self.message
 
-    def validate_catalog_contents(self):
+
+    def validate_catalog_contents(self, verbose_dict, summary_dict, summary_lock):
         """
         Validates contents of current catalog
         :return: list of child messages
         """
-        messages = []
+        jobs = []
         for link in self.stac_file["links"]:
             if link["rel"] in ["child", "item"]:
                 child_url = urljoin(str(self.fpath), link["href"])
-                stac = StacValidate(child_url.replace("///", "//"), self.stac_version)
-                messages.append(stac.message)
+                p = multiprocessing.Process(target=StacValidate, args=(child_url.replace("///", "//"), verbose_dict, summary_dict, summary_lock, self.stac_version))
+                jobs.append(p)
+                time.sleep(0.2)
+                p.start()
 
-                self.status["catalogs"]["valid"] += stac.status["catalogs"]["valid"]
-                self.status["catalogs"]["invalid"] += stac.status["catalogs"]["invalid"]
-                self.status["items"]["valid"] += stac.status["items"]["valid"]
-                self.status["items"]["invalid"] += stac.status["items"]["invalid"]
+        for j in jobs:
+            j.join()
 
-        # print('stat', self.status)
-        return messages
 
-    def run(self):
+    def run(self, verbose_dict, summary_dict, summary_lock):
         """
         Entry point
         :return: message json
         """
         try:
-            self.stac_file = requests.get(self.stac_file).json()
+            r = requests.get(self.path)
+            self.stac_file = r.json()
+
         except requests.exceptions.MissingSchema as e:
             with open(self.stac_file) as f:
                 data = json.load(f)
@@ -118,36 +120,39 @@ class StacValidate:
 
         if "catalog" in self.fpath.stem:
             self.message["asset_type"] = "catalog"
-            self.validate_stac(self.stac_file, self.CATALOG_SCHEMA)
+            self.validate_stac(self.stac_file, self.CATALOG_SCHEMA, verbose_dict, summary_dict, summary_lock)
 
             if self.message["valid_stac"]:
                 self.status["catalogs"]["valid"] += 1
             else:
                 self.status["catalogs"]["invalid"] += 1
 
-            self.message['children'] = self.validate_catalog_contents()
+            self.validate_catalog_contents(verbose_dict, summary_dict, summary_lock)
+
         else:
             self.message["asset_type"] = "item"
-            self.validate_stac(self.stac_file, self.ITEM_SCHEMA)
+            self.validate_stac(self.stac_file, self.ITEM_SCHEMA, verbose_dict, summary_dict, summary_lock)
 
             if self.message["valid_stac"]:
                 self.status["items"]["valid"] += 1
             else:
                 self.status["items"]["invalid"] += 1
 
-        self.message['path'] = str(self.fpath)
-
-        return json.dumps(self.message)
-
-
 def main(args):
     stac_file = args.get('<stac_file>')
     version = args.get('--version')
     verbose = args.get('--verbose')
-    stac = StacValidate(stac_file, version, verbose)
+
+    manager = multiprocessing.Manager()
+    verbose_dict = manager.dict()
+    summary_dict = manager.dict(lock=True)
+    summary_lock = multiprocessing.Lock()
+
+    stac = StacValidate(stac_file, verbose_dict, summary_dict, summary_lock,  version, verbose)
 
     if verbose:
-        print(json.dumps(stac.message, indent=4))
+        print(json.dumps(verbose_dict.copy(), indent=4))
+        print(len(verbose_dict.keys()))
     else:
         print(json.dumps(stac.status, indent=4))
 
@@ -155,8 +160,12 @@ def main(args):
 
 if __name__ == "__main__":
     args = docopt(__doc__)
+    import time
     try:
+        start = time.time()
         main(args)
+        end = time.time()
+        print(end - start)
         retval = 0
     except Exception as e:
         traceback.print_exc()
