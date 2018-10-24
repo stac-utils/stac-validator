91ed666a39b955e984ed181b1cbd077ee30eb214
James Banting <jbanting@spakgeo.com>
2018-10-23 22:30:58 -0600

    added validation for collections

--- stac_validator.py
+++ stac_validator.py
@@ -16,6 +16,7 @@ Options:
 __author__ = "James Banting, Alex Mandel, Guillaume Morin, Darren Wiens"
 
 import os
+import shutil
 from pathlib import Path
 import tempfile
 from urllib.parse import urljoin
@@ -27,91 +28,100 @@ import requests
 from docopt import docopt
 
 from cachetools import cached, TTLCache
+
 cache = TTLCache(maxsize=10, ttl=900)
 
 
+class SpecFile:
+    pass
+
+
 class StacValidate:
-    def __init__(self, stac_file, version="master", verbose=False):
+    def __init__(self, stac_file, version="master", depth=0):
         """
         Validate a STAC file
         :param stac_file: file to validate
         :param version: github tag - defaults to master
         """
         if version is None:
-            version = 'master'
+            version = "master"
 
         self.stac_version = version
+        self.depth = depth
         self.stac_file = stac_file.strip()
-        self.ITEM_SCHEMA, self.ITEM_GEOSJON_SCHEMA, self.CATALOG_SCHEMA = self.get_specs(self.stac_version)
+        self.dirpath = ""
+        self.ITEM_SCHEMA, self.ITEM_GEOSJON_SCHEMA, self.CATALOG_SCHEMA = self.get_specs(
+            self.stac_version
+        )
         self.fpath = Path(stac_file)
         self.message = {}
         self.status = {
             "catalogs": {"valid": 0, "invalid": 0},
+            "collections": {"valid": 0, "invalid": 0},
             "items": {"valid": 0, "invalid": 0},
         }
-
         self.run()
 
-    @staticmethod
     @cached(cache)
-    def get_specs(version):
+    def get_specs(self, version):
         """
         Get the versions from github. Cache them if possible.
         :return: specs
         """
         # old versions have a different path to schema
-        old_versions = ['v0.4.0', 'v0.4.1', 'v0.5.0', 'v0.5.1', 'v0.5.2']
+        old_versions = ["v0.4.0", "v0.4.1", "v0.5.0", "v0.5.1", "v0.5.2"]
 
+        # Collection spec can be validated by catalog spec.
         if version in old_versions:
             CATALOG_SCHEMA_URL = (
-                    "https://raw.githubusercontent.com/radiantearth/stac-spec/"
-                    + version
-                    + "/static-catalog/json-schema/catalog.json"
+                "https://raw.githubusercontent.com/radiantearth/stac-spec/"
+                + version
+                + "/static-catalog/json-schema/catalog.json"
             )
             ITEM_SCHEMA_URL = (
-                    "https://raw.githubusercontent.com/radiantearth/stac-spec/"
-                    + version
-                    + "/json-spec/json-schema/stac-item.json"
+                "https://raw.githubusercontent.com/radiantearth/stac-spec/"
+                + version
+                + "/json-spec/json-schema/stac-item.json"
             )
             ITEM_GEOJSON_SCHEMA_URL = (
-                    "https://raw.githubusercontent.com/radiantearth/stac-spec/"
-                    + version
-                    + "/json-spec/json-schema/geojson.json"
+                "https://raw.githubusercontent.com/radiantearth/stac-spec/"
+                + version
+                + "/json-spec/json-schema/geojson.json"
             )
         else:
             CATALOG_SCHEMA_URL = (
-                    "https://raw.githubusercontent.com/radiantearth/stac-spec/"
-                    + version
-                    + "/catalog-spec/json-schema/catalog.json"
+                "https://raw.githubusercontent.com/radiantearth/stac-spec/"
+                + version
+                + "/catalog-spec/json-schema/catalog.json"
             )
             ITEM_SCHEMA_URL = (
-                    "https://raw.githubusercontent.com/radiantearth/stac-spec/"
-                    + version
-                    + "/item-spec/json-schema/stac-item.json"
+                "https://raw.githubusercontent.com/radiantearth/stac-spec/"
+                + version
+                + "/item-spec/json-schema/stac-item.json"
             )
             ITEM_GEOJSON_SCHEMA_URL = (
-                    "https://raw.githubusercontent.com/radiantearth/stac-spec/"
-                    + version
-                    + "/item-spec/json-schema/geojson.json"
+                "https://raw.githubusercontent.com/radiantearth/stac-spec/"
+                + version
+                + "/item-spec/json-schema/geojson.json"
             )
 
         # need to make a temp local file for geojson.
-        dirpath = tempfile.mkdtemp()
+        self.dirpath = tempfile.mkdtemp()
 
         stac_item_geojson = requests.get(ITEM_GEOJSON_SCHEMA_URL).json()
         stac_item = requests.get(ITEM_SCHEMA_URL).json()
         stac_catalog = requests.get(CATALOG_SCHEMA_URL).json()
 
-        with open(os.path.join(dirpath, 'geojson.json'), 'w') as fp:
+        with open(os.path.join(self.dirpath, "geojson.json"), "w") as fp:
             fp.write(json.dumps(stac_item_geojson))
-        with open(os.path.join(dirpath, 'stac-item.json'), 'w') as fp:
+        with open(os.path.join(self.dirpath, "stac-item.json"), "w") as fp:
             fp.write(json.dumps(stac_item))
-        with open(os.path.join(dirpath, 'stac-catalog.json'), 'w') as fp:
+        with open(os.path.join(self.dirpath, "stac-catalog.json"), "w") as fp:
             fp.write(json.dumps(stac_catalog))
 
-        ITEM_SCHEMA = os.path.join(dirpath, 'stac-item.json')
-        ITEM_GEOJSON_SCHEMA = os.path.join(dirpath, 'geojson.json')
-        CATALOG_SCHEMA = os.path.join(dirpath, 'stac-catalog.json')
+        ITEM_SCHEMA = os.path.join(self.dirpath, "stac-item.json")
+        ITEM_GEOJSON_SCHEMA = os.path.join(self.dirpath, "geojson.json")
+        CATALOG_SCHEMA = os.path.join(self.dirpath, "stac-catalog.json")
 
         return ITEM_SCHEMA, ITEM_GEOJSON_SCHEMA, CATALOG_SCHEMA
 
@@ -126,8 +136,10 @@ class StacValidate:
         with open(schema) as fp:
             stac_schema = json.load(fp)
 
-        path_to_schema_dir = os.path.abspath(os.path.dirname(schema))
-        geosjson_resolver = RefResolver(base_uri='file://'+path_to_schema_dir+'/', referrer='geojson.json')
+        path_to_schema_dir = os.path.abspath(self.dirpath)
+        geosjson_resolver = RefResolver(
+            base_uri="file://" + path_to_schema_dir + "/", referrer="geojson.json"
+        )
 
         try:
             validate(stac_file, stac_schema)
@@ -139,7 +151,7 @@ class StacValidate:
             try:
                 validate(stac_file, stac_schema, resolver=geosjson_resolver)
                 self.message["valid_stac"] = True
-            except:
+            except Exception as error:
                 self.message["valid_stac"] = False
                 self.message["error"] = f"{error.args}"
         except ValidationError as error:
@@ -147,7 +159,7 @@ class StacValidate:
             self.message["error"] = f"{error.message} of {list(error.path)}"
 
         except Exception as error:
-            self.message["valid_stac"] = False
+            self.message["valid_staselfc"] = False
             self.message["error"] = f"{error}"
 
     def validate_catalog_contents(self):
@@ -156,19 +168,17 @@ class StacValidate:
         :return: list of child messages
         """
         messages = []
-        depth_counter = 0
         for link in self.stac_file["links"]:
             if link["rel"] in ["child", "item"]:
-                depth_counter += 1
+                self.depth += 1
                 child_url = urljoin(str(self.fpath), link["href"])
-                stac = StacValidate(child_url.replace("///", "//"), self.stac_version)
+                stac = self.validate_stac(child_url.replace("///", "//"))
                 messages.append(stac.message)
 
                 self.status["catalogs"]["valid"] += stac.status["catalogs"]["valid"]
                 self.status["catalogs"]["invalid"] += stac.status["catalogs"]["invalid"]
                 self.status["items"]["valid"] += stac.status["items"]["valid"]
                 self.status["items"]["invalid"] += stac.status["items"]["invalid"]
-        print(depth_counter)
         return messages
 
     def run(self):
@@ -176,6 +186,10 @@ class StacValidate:
         Entry point
         :return: message json
         """
+
+        Collections_Fields = ['keywords','license','title','provider','version','description','stac_version']
+
+        # URL or file
         try:
             self.stac_file = requests.get(self.stac_file).json()
         except requests.exceptions.MissingSchema as e:
@@ -188,7 +202,9 @@ class StacValidate:
             self.status = self.message
             return json.dumps(self.message)
 
+        # Check STAC Type
         if "catalog" in self.fpath.stem:
+            # Congratulations, It's a Catalog!
             self.message["asset_type"] = "catalog"
             self.validate_stac(self.stac_file, self.CATALOG_SCHEMA)
 
@@ -196,9 +212,21 @@ class StacValidate:
                 self.status["catalogs"]["valid"] += 1
             else:
                 self.status["catalogs"]["invalid"] += 1
+            print(self.fpath)
+            self.message["children"] = self.validate_catalog_contents()
+        elif any(field in Collections_Fields for field in self.stac_file.keys()):
+            # Congratulations, It's a Collection!
+            self.message["asset_type"] = "collection"
+            self.validate_stac(self.stac_file, self.CATALOG_SCHEMA)
 
+            if self.message["valid_stac"]:
+                self.status["collections"]["valid"] += 1
+            else:
+                self.status["collections"]["invalid"] += 1
+            print(self.fpath)
             self.message["children"] = self.validate_catalog_contents()
         else:
+            # Congratulations, It's an Item!
             self.message["asset_type"] = "item"
             self.validate_stac(self.stac_file, self.ITEM_SCHEMA)
             if self.message["valid_stac"]:
@@ -216,7 +244,7 @@ def main(args):
     version = args.get("--version")
     verbose = args.get("--verbose")
     stac = StacValidate(stac_file, version, verbose)
-
+    shutil.rmtree(stac.dirpath)
     if verbose:
         print(json.dumps(stac.message, indent=4))
     else:
