0067606401c12b4f650120618a0d80fe09d3fe8c
JamesOConnor <oconnoj4@tcd.ie>
2019-09-01 11:44:15 +0100

    Update the test paths, show everything still works

--- tests/test_stac_validator.py
+++ tests/test_stac_validator.py
@@ -8,7 +8,7 @@ from stac_validator import stac_validator
 
 
 def _run_validate(
-    url, stac_spec_dirs=None, version="master", log_level="DEBUG", follow=False
+        url, stac_spec_dirs=None, version="master", log_level="DEBUG", follow=False
 ):
     stac = stac_validator.StacValidate(url, stac_spec_dirs, version, log_level, follow)
     stac.run()
@@ -33,39 +33,39 @@ def test_item_v061_remote():
 
 @pytest.mark.item
 def test_good_item_validation_v052_verbose():
-    stac = _run_validate(url="tests/test_data/good_item_v052.json", version="v0.5.2")
+    stac = _run_validate(url="tests/test_data/examples/v0.5.2/json-spec/examples/planet-sample.json", version="v0.5.2")
     assert stac.message == [
         {
             "asset_type": "item",
             "valid_stac": True,
             "error_message": None,
-            "path": "tests/test_data/good_item_v052.json",
+            "path": "tests/test_data/examples/v0.5.2/json-spec/examples/planet-sample.json",
         }
     ]
 
 
 @pytest.mark.item
 def test_good_item_validation_v060_verbose():
-    stac = _run_validate(url="tests/test_data/good_item_v060.json", version="v0.6.0")
+    stac = _run_validate(url="tests/test_data/examples/v0.6.0/item-spec/examples/planet-sample.json", version="v0.6.0")
     assert stac.message == [
         {
             "asset_type": "item",
             "valid_stac": True,
             "error_message": None,
-            "path": "tests/test_data/good_item_v060.json",
+            "path": "tests/test_data/examples/v0.6.0/item-spec/examples/planet-sample.json",
         }
     ]
 
 
 @pytest.mark.item
 def test_good_item_validation_v061_verbose():
-    stac = _run_validate(url="tests/test_data/good_item_v061.json", version="v0.6.1")
+    stac = _run_validate(url="tests/test_data/examples/v0.6.1/item-spec/examples/planet-sample.json", version="v0.6.1")
     assert stac.message == [
         {
             "asset_type": "item",
             "valid_stac": True,
             "error_message": None,
-            "path": "tests/test_data/good_item_v061.json",
+            "path": "tests/test_data/examples/v0.6.1/item-spec/examples/planet-sample.json",
         }
     ]
 
@@ -74,16 +74,16 @@ def test_good_item_validation_v061_verbose():
 @pytest.mark.item
 def test_local_schema_item():
     stac = _run_validate(
-        url="tests/test_data/good_item_v061.json",
+        url="tests/test_data/examples/v0.6.1/item-spec/examples/planet-sample.json",
         version="v0.6.1",
-        stac_spec_dirs="tests/test_data/local_schema/item_v061/json-schema",
+        stac_spec_dirs="schemas/v0.6.1/item-spec/json-schema",
     )
     assert stac.message == [
         {
             "asset_type": "item",
             "valid_stac": True,
             "error_message": None,
-            "path": "tests/test_data/good_item_v061.json",
+            "path": "tests/test_data/examples/v0.6.1/item-spec/examples/planet-sample.json",
         }
     ]
 
@@ -106,13 +106,14 @@ def test_catalog_v061_remote():
 
 @pytest.mark.catalog
 def test_good_catalog_validation_v052_verbose():
-    stac = _run_validate(url="tests/test_data/good_catalog_v052.json", version="v0.5.2")
+    stac = _run_validate(url="tests/test_data/examples/v0.5.2/json-spec/examples/catalog.json",
+                         version="v0.5.2")
     assert stac.message == [
         {
             "asset_type": "catalog",
             "valid_stac": True,
             "error_message": None,
-            "path": "tests/test_data/good_catalog_v052.json",
+            "path": "tests/test_data/examples/v0.5.2/json-spec/examples/catalog.json",
         }
     ]
 
@@ -149,33 +150,34 @@ def test_nested_catalog_v052():
 @pytest.mark.catalog
 def test_local_schema_catalog():
     stac = _run_validate(
-        url="tests/test_data/good_catalog_v061.json",
+        url="tests/test_data/examples/v0.6.1/catalog-spec/examples/catalog.json",
         version="v0.6.1",
-        stac_spec_dirs="tests/test_data/local_schema/catalog_v061/json-schema",
+        stac_spec_dirs="schemas/v0.6.1/catalog-spec/json-schema",
     )
     assert stac.message == [
         {
             "asset_type": "catalog",
             "valid_stac": True,
             "error_message": None,
-            "path": "tests/test_data/good_catalog_v061.json",
+            "path": "tests/test_data/examples/v0.6.1/catalog-spec/examples/catalog.json",
         }
     ]
+
 
 @pytest.mark.local_schema
 @pytest.mark.catalog
 def test_local_schema_catalog_schema_fail():
     stac = _run_validate(
-        url="tests/test_data/good_catalog_v052.json",
+        url="tests/test_data/examples/v0.5.2/json-spec/examples/catalog.json",
         version="v0.6.1",
-        stac_spec_dirs="tests/test_data/local_schema/catalog_v061/json-schema",
+        stac_spec_dirs="schemas/v0.6.1/catalog-spec/json-schema",
     )
     assert stac.message == [
         {
             "asset_type": "catalog",
             "valid_stac": False,
             "error_message": "'stac_version' is a required property of []",
-            "path": "tests/test_data/good_catalog_v052.json",
+            "path": "tests/test_data/examples/v0.5.2/json-spec/examples/catalog.json",
         }
     ]
 
@@ -200,16 +202,16 @@ def test_collection_v061_remote():
 @pytest.mark.catalog
 def test_local_schema_collection():
     stac = _run_validate(
-        url="tests/test_data/good_collection_v061.json",
+        url="tests/test_data/examples/v0.6.1/collection-spec/examples/sentinel2.json",
         version="v0.6.1",
-        stac_spec_dirs="tests/test_data/local_schema/collection_v061/json-schema",
+        stac_spec_dirs="schemas/v0.6.1/collection-spec/json-schema",
     )
     assert stac.message == [
         {
             "asset_type": "collection",
             "valid_stac": True,
             "error_message": None,
-            "path": "tests/test_data/good_collection_v061.json",
+            "path": "tests/test_data/examples/v0.6.1/collection-spec/examples/sentinel2.json",
         }
     ]
 
@@ -238,17 +240,19 @@ def test_bad_url():
         }
     ]
 
+
 @pytest.mark.validator
 @pytest.mark.local_schema
 @pytest.mark.catalog
 def test_local_schema_catalog_wrong_schema():
     with pytest.raises(SystemExit) as e:
         stac = _run_validate(
-            url="tests/test_data/good_catalog_v052.json",
+            url="tests/test_data/examples/v0.5.2/json-spec/examples/CBERS_4_MUX_20170528_090_084_L2.json",
             version="v0.6.1",
-            stac_spec_dirs="tests/test_data/local_schema/item_v061/json-schema",
+            stac_spec_dirs="schemas/v0.5.2/json-spec/json-schema",
         )
         assert e.value.code == 1
+
 
 @pytest.mark.validator
 @pytest.mark.local_schema
@@ -257,11 +261,12 @@ def test_local_schema_catalog_wrong_schema():
 def test_multiple_local_schema_catalog_wrong_schema():
     with pytest.raises(SystemExit) as e:
         stac = _run_validate(
-            url="tests/test_data/good_catalog_v061.json",
-            stac_spec_dirs="tests/test_data/local_schema/item_v061/json-schem,tests/test_data/local_schema/catalog_v061/json-schem",
+            url="tests/test_data/examples/v0.6.1/catalog-spec/examples/catalog.json",
+            stac_spec_dirs="this/is/a/fake/directory",
             version="v0.6.1",
         )
         assert e.value.code == 1
+
 
 @pytest.mark.validator
 @pytest.mark.local_schema
@@ -269,8 +274,8 @@ def test_multiple_local_schema_catalog_wrong_schema():
 @pytest.mark.catalog
 def test_multiple_local_schemas_catalog():
     stac = _run_validate(
-        url="tests/test_data/good_catalog_v061.json",
-        stac_spec_dirs="tests/test_data/local_schema/item_v061/json-schema,tests/test_data/local_schema/catalog_v061/json-schema",
+        url="tests/test_data/examples/v0.6.1/catalog-spec/examples/catalog.json",
+        stac_spec_dirs="schemas/v0.6.1/item-spec/json-schema,schemas/v0.6.1/catalog-spec/json-schema",
         version="v0.6.1",
     )
     assert stac.status == {
@@ -280,15 +285,15 @@ def test_multiple_local_schemas_catalog():
         "unknown": 0,
     }
 
+
 @pytest.mark.validator
 @pytest.mark.local_schema
 @pytest.mark.multiple_dirs
 @pytest.mark.catalog
 def test_multiple_local_schemas_nested_catalog():
-
     stac = _run_validate(
         url="tests/test_data/nested_catalogs/parent_catalog.json",
-        stac_spec_dirs="tests/test_data/local_schema/item_v052,tests/test_data/local_schema/catalog_v052",
+        stac_spec_dirs="schemas/v0.5.2/json-spec/json-schema,schemas/v0.5.2/static-catalog/json-schema",
         version="v0.5.2",
         follow=True,
     )
