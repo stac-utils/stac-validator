70b1789f6128b11e37880c1235f4b60e5c05fe5c
jonhealy1 <jonathan.d.healy@gmail.com>
2020-09-02 22:49:31 -0700

    troubleshoot setup for pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 req_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "requirements.txt")
 
-__version__ = open("./VERSION", "r").read().strip()
+__version__ = '1.0.0'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -34,6 +34,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sparkgeo/stac-validator",
+    download_url="https://github.com/sparkgeo/stac-validator/archive/v1.0.0.tar.gz",
     install_requires=requires,
     packages=["stac_validator"],
     entry_points={"console_scripts": ["stac_validator = stac_validator.stac_validator:main"]},
