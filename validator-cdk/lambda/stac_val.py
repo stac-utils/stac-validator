dcc3f58353135f5409894ee9543ad4754bb8a3b8
jonhealy1 <jonathan.d.healy@gmail.com>
2021-04-14 09:11:57 -0700

    updt responses

--- validator-cdk/lambda/stac_val.py
+++ validator-cdk/lambda/stac_val.py
@@ -4,9 +4,8 @@ from stac_validator import stac_validator
 
 
 def handler(event, context):
-    body = {}
-    body["event"] = event
-    wanted = body["event"]["body"].split(" ")
+
+    wanted = event["body"].split(" ")
     want = str(wanted[3])
     val_file = want[1:-3]
     stac = stac_validator.StacValidate(val_file)
