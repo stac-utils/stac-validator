0a96870d63a45a6c534126d592376bbab150ed52
jonhealy1 <jonathan.d.healy@gmail.com>
2021-04-21 13:03:54 -0700

    add pytest mypy

--- setup.py
+++ setup.py
@@ -35,11 +35,10 @@ setup(
     download_url="https://github.com/sparkgeo/stac-validator/archive/v2.0.0.tar.gz",
     install_requires=[
         "requests>=2.19.1",
-        "pytest==6.2.2",
         "jsonschema==3.2.0",
         "pystac==0.5.6",
         "click==7.1.2",
-        "pre-commit==1.21.0",
+        "pre-commit==2.9.0",
         "tox==3.23.0",
     ],
     packages=["stac_validator"],
