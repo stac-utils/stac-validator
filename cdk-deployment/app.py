a9af115d2463db484114fb94ad09871732947fe6
Jonathan Healy <jonathan.d.healy@gmail.com>
2021-04-26 21:55:42 -0700

    Update app.py

--- cdk-deployment/app.py
+++ cdk-deployment/app.py
@@ -5,9 +5,9 @@
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
-from validator_cdk.validator_cdk_stack import FastAPICdkStack
+from validator_cdk.validator_cdk_stack import ValidatorCdkStack
 
 app = core.App()
-FastAPICdkStack(app, "FastAPICdkStack")
+ValidatorCdkStack(app, "ValidatorCdkStack")
 
 app.synth()
