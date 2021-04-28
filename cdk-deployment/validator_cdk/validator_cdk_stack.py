ac758b70bb4065453b48756d4f8b37bc1f61ec75
jonhealy1 <jonathan.d.healy@gmail.com>
2021-04-28 14:20:03 -0700

    change stack name for testing

--- cdk-deployment/validator_cdk/validator_cdk_stack.py
+++ cdk-deployment/validator_cdk/validator_cdk_stack.py
@@ -2,13 +2,14 @@
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
+from pathlib import Path
+
 from aws_cdk import aws_apigateway as apigw
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import core as cdk
-from pathlib import Path
 
 
-class ValidatorCdkStack(cdk.Stack):
+class ValidatorTestStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
@@ -18,7 +19,7 @@ class ValidatorCdkStack(cdk.Stack):
         # Defines an AWS Lambda resource
         validator_lambda = _lambda.Function(
             self,
-            "STACValidator",
+            "TESTValidator",
             runtime=_lambda.Runtime.PYTHON_3_8,
             code=_lambda.Code.from_docker_build(
                 path=str(build_path.resolve()),
@@ -32,7 +33,7 @@ class ValidatorCdkStack(cdk.Stack):
 
         apigw.LambdaRestApi(
             self,
-            "Endpoint",
+            "TestEndpoint",
             handler=validator_lambda,
             default_cors_preflight_options=cors,
         )
