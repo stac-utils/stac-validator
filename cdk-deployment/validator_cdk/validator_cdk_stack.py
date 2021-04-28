b803dff3c0d11e76d2093fab9308f9bb9ad1e08e
Mike Connor <mike@sparkgeo.com>
2021-04-28 12:38:22 -0700

    Build deps in docker container.

--- cdk-deployment/validator_cdk/validator_cdk_stack.py
+++ cdk-deployment/validator_cdk/validator_cdk_stack.py
@@ -5,28 +5,27 @@
 from aws_cdk import aws_apigateway as apigw
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import core as cdk
+from pathlib import Path
 
 
 class ValidatorCdkStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        val_lib = _lambda.LayerVersion(
-            self,
-            "validator-library-layer",
-            code=_lambda.AssetCode("build-libraries/libraries.zip"),
-            compatible_runtimes=[_lambda.Runtime.PYTHON_3_8],
-        )
+        build_path = Path("../")
+        print(str(build_path.resolve()))
 
         # Defines an AWS Lambda resource
         validator_lambda = _lambda.Function(
             self,
             "STACValidator",
             runtime=_lambda.Runtime.PYTHON_3_8,
-            code=_lambda.Code.asset("lambda"),
+            code=_lambda.Code.from_docker_build(
+                path=str(build_path.resolve()),
+                file="cdk-deployment/lambda/Dockerfile",
+            ),
             handler="lambda.handler",
             timeout=cdk.Duration.seconds(30),
-            layers=[val_lib],
         )
 
         cors = apigw.CorsOptions(allow_origins=["*"])
