a7b4eb4414b3f37235bc8e480d4775c826d1d8f4
Tim Schaub <tim@planet.com>
2022-03-10 11:05:30 -0700

    Exit with non-zero code when validation fails

--- tests/test_sys_exit.py
+++ tests/test_sys_exit.py
@@ -11,6 +11,22 @@ def test_correct_sys_exit_error_python():
         )
 
 
+def test_correct_sys_exit_error_recursion():
+    try:
+        subprocess.run(
+            [
+                "stac_validator",
+                "tests/test_data/v100/catalog-with-bad-item.json",
+                "--recursive",
+                "-1",
+            ],
+            check=True,
+        )
+        assert False
+    except subprocess.CalledProcessError:
+        assert True
+
+
 def test_false_sys_exit_error_python():
     subprocess.run(
         ["stac-validator", "tests/test_data/v090/items/good_item_v090.json"],
