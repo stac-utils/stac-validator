7285a5b10eb7d9ff547b646cf87a1360d2871a36
jonhealy1 <jonathan.d.healy@gmail.com>
2022-04-28 05:06:21 -0700

    version = 3.1.0

--- stac_validator/stac_validator.py
+++ stac_validator/stac_validator.py
@@ -49,7 +49,7 @@ from .validate import StacValidate
     default="",
     help="Save full recursive output to log file (local filepath).",
 )
-@click.version_option(version="3.0.0")
+@click.version_option(version="3.1.0")
 def main(
     stac_file,
     recursive,
