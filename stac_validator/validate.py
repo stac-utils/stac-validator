dbe143579be0297c729fcf1df1543a4ab2b5bd07
jonhealy1 <jonathan.d.healy@gmail.com>
2023-02-16 17:47:38 +0300

    add more schemas

--- stac_validator/validate.py
+++ stac_validator/validate.py
@@ -186,8 +186,10 @@ class StacValidate:
 
     def core_validator(self, stac_type: str):
         stac_type = stac_type.lower()
-        if stac_type == "item" and self.version in ["0.9.0", "1.0.0"]:
-            self.schema = f"stac_validator/schemas/v{self.version}/item.json"
+        if stac_type == "item" and self.version in ["0.8.0", "0.9.0", "1.0.0"]:
+            self.schema = f"stac_validator/schemas/v{self.version}/{stac_type}.json"
+        elif stac_type == "collection" and self.version in ["0.9.0", "1.0.0-beta.1"]:
+            self.schema = f"stac_validator/schemas/v{self.version}/{stac_type}.json"
         else:
             self.schema = set_schema_addr(self.version, stac_type)
         self.custom_validator()
