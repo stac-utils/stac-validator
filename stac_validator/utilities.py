3d334639883c4f1f11e4fe0582bb3a2777423689
jonhealy1 <jonathan.d.healy@gmail.com>
2023-02-17 23:43:07 +0300

    fetch and parse file

--- stac_validator/utilities.py
+++ stac_validator/utilities.py
@@ -63,14 +63,35 @@ def get_stac_type(stac_content) -> str:
 
 
 def fetch_and_parse_file(input_path) -> dict:
-    if is_valid_url(input_path):
-        resp = requests.get(input_path)
-        data = resp.json()
-    else:
-        with open(input_path) as f:
-            data = json.load(f)
+    """Fetches and parses a JSON file from a URL or local file.
 
-    return data
+    Given a URL or local file path to a JSON file, this function fetches the file,
+    and parses its contents into a dictionary. If the input path is a valid URL, the
+    function uses the requests library to download the file, otherwise it opens the
+    local file with the json library.
+
+    Args:
+        input_path: A string representing the URL or local file path to the JSON file.
+
+    Returns:
+        A dictionary containing the parsed contents of the JSON file.
+
+    Raises:
+        ValueError: If the input is not a valid URL or local file path.
+        requests.exceptions.RequestException: If there is an error while downloading the file.
+    """
+    try:
+        if is_url(input_path):
+            resp = requests.get(input_path)
+            resp.raise_for_status()
+            data = resp.json()
+        else:
+            with open(input_path) as f:
+                data = json.load(f)
+
+        return data
+    except (ValueError, requests.exceptions.RequestException) as e:
+        raise e
 
 
 @functools.lru_cache(maxsize=48)
