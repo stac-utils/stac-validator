8926516a8035db7f5e52271af7747a1a8dbed8ab
jonhealy1 <jonathan.d.healy@gmail.com>
2026-07-30 21:53:33 +0800

    fix: Use stable historical collection instead of NRT for item_collection test

--- tests/test_validate_item_collection.py
+++ tests/test_validate_item_collection.py
@@ -544,7 +544,9 @@ def test_validate_item_collection_remote_pages():
 
 
 def test_validate_item_collection_remote_pages_1_v110():
-    stac_file = "https://stac.dataspace.copernicus.eu/v1/collections/sentinel-3-olci-2-wfr-nrt/items"
+    # Use a permanent historical collection (sentinel-2-l2a) instead of NRT
+    # NRT collections are dynamic and may have fewer items at test time
+    stac_file = "https://stac.dataspace.copernicus.eu/v1/collections/sentinel-2-l2a/items"
     stac = stac_validator.StacValidate(stac_file, item_collection=True, pages=1)
     stac.validate_item_collection()
 
